5f01b25f6ca1fd0c037ba328425319c3908a0694
a.bubelev <bubeleu@gmail.com>
2022-06-01 13:59:11 +0300

    create todoform

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const ToDo = () => {
+    
     return (
         <div>I'm Todo</div>
     )
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-const ToDoForm = () => {
+const ToDoForm = (props) => {
+
+    const [userInput, setUserInput] = useState('');
+
+    const handleChange = (e) => {
+        setUserInput(e.currentTarget.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.addTask(userInput);
+        setUserInput('');
+    };
+
+    const handlePress = (e) => {
+        if (e.key === 'Enter') {
+            handleSubmit(e);
+        }
+    }
 
     return (
-        <div>Hello ToDoForms</div>
+        <>
+            <div>Hello ToDoForms</div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    value={userInput}
+                    type='text'
+                    onChange={handleChange}
+                    onKeyDown={handlePress}
+                ></input>
+                <button>Save</button>
+            </form>
+        </>
     );
 };
 export default ToDoForm;
