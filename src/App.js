3604bbd8ede0bd3e5e26e05a6ae691e2d16ed865
a.bubelev <bubeleu@gmail.com>
2022-06-01 13:01:39 +0300

    create App with functions(add, remove, toggle)

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useState } from "react";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const addTask = (id) => {
+
+  };
+  const removeTask = (id) => {
+
+  };
+  const toggle = (id) => {
+
+  };
+  return (
+    <div>Hello Todo</div>
+  )
+}
+export default App;
