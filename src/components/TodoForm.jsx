f5e26f16a2583364079fa13f12d361e70ffb1234
a.bubelev <bubeleu@gmail.com>
2022-06-02 14:02:16 +0300

    ready Todo

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from "react";
 
-const ToDoForm = ({addTask}) => {
+const ToDoForm = ({ addTask }) => {
 
     const [userInput, setUserInput] = useState('');
-//current value
+    //current value
     const handleChange = (e) => {
         setUserInput(e.currentTarget.value);
     };
-//
+    //
     const handleSubmit = (e) => {
         e.preventDefault();
         addTask(userInput);
         setUserInput('');
     };
-//
+    //
     const handlePress = (e) => {
         if (e.key === 'Enter') {
             handleSubmit(e);
@@ -22,7 +22,6 @@ const ToDoForm = ({addTask}) => {
 
     return (
         <>
-            <div>Hello ToDoForms</div>
             <form onSubmit={handleSubmit}>
                 <input
                     value={userInput}
